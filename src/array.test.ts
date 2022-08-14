e67779d55318e897bdccab936a8c1fed619e7291
t104fk <t104fk@gmail.com>
2022-08-14 23:26:34 +0900

    unique

--- src/array.test.ts
+++ src/array.test.ts
@@ -1,4 +1,4 @@
-import { swap } from "./array";
+import { swap, unique } from "./array";
 
 test("Swapping test", () => {
   const sut = {
@@ -9,4 +9,12 @@ test("Swapping test", () => {
   expect(Object.keys(swapped).length).toBe(2);
   expect(swapped["Water"]).toBe("en");
   expect(swapped["水"]).toBe("ja");
+});
+
+test("Unique test", () => {
+  const sut = [1, 2, 3, 1, "1"];
+  const result = unique(sut);
+  expect(result.length).toBe(4);
+  expect(result[0]).toBe(1);
+  expect(result[3]).toBe("1");
 });
--- src/array.ts
+++ src/array.ts
@@ -1,3 +1,4 @@
 type Swappable = Record<string, string>;
 export const swap = <T extends Swappable>(obj: T) =>
   Object.fromEntries(Object.entries(obj).map(([k, v]) => [v, k]));
+export const unique = <T>(arr: T[]) => [...new Set(arr)];
